cb22ea1767e09ef40e2f255f07ab9b26a80cf55e
Viktor Berezovskyi <64471555+colorfulmdx2@users.noreply.github.com>
2021-03-15 20:00:41 +0300

    feat: end up human readable numbers

--- src/index.js
+++ src/index.js
@@ -1,3 +1,55 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    const numbers = [
+        "zero",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    const tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    const wordsMaker = (number) => {
+        if (number < 20) return numbers[number];
+        let digit = number % 10;
+        if (number < 100)
+            return tens[Math.floor(number / 10)] + (digit ? " " + numbers[digit] : "");
+        if (number < 1000)
+            return (
+                numbers[Math.floor(number / 100)] +
+                " hundred" +
+                (number % 100 === 0 ? "" : " " + wordsMaker(number % 100))
+            );
+        return (
+            wordsMaker(Math.floor(number / 1000)) +
+            " thousand" +
+            (number % 1000 !== 0 ? "" + wordsMaker(number % 1000) : " ")
+        );
+    }
+    return wordsMaker(number)
 }
